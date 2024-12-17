592efd9268d9cddf40b37f93cf4be0896b337a36
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2024-12-17 15:16:01 +0100

    chore: Add skeleton

---
+++ src/main/java/de/neuefische/hhjava245rickandmorty/HhJava245RickandmortyApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.hhjava245rickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava245RickandmortyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava245RickandmortyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/hhjava245rickandmorty/HhJava245RickandmortyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.hhjava245rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava245RickandmortyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
