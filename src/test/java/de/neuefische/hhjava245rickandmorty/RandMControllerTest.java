b45adcddb19148690e68c1aa2f4f3bb24c445b4a
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2024-12-18 10:48:25 +0100

    test: Add integration test for getCharacters

---
+++ src/test/java/de/neuefische/hhjava245rickandmorty/RandMControllerTest.java
@@ -0,0 +1,60 @@
+package de.neuefische.hhjava245rickandmorty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureMockRestServiceServer
+class RandMControllerTest {
+
+    @Autowired
+    private MockMvc mm;
+
+    @Autowired
+    private MockRestServiceServer ms;
+
+    @Test
+    void getCharacters() throws Exception {
+        ms.expect(requestTo("https://rickandmortyapi.com/api/character"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess("""
+                                {
+                                    "info": {
+                                        "count": 32,
+                                        "pages": 42
+                                    },
+                                    "results": [
+                                        {
+                                            "id": 1,
+                                            "name": "Rick Sanchez",
+                                            "status": "Alive",
+                                            "species": "Human"
+                                        }
+                                    ]
+                                }
+                                """,
+                        MediaType.APPLICATION_JSON));
+
+        mm.perform(get("/api/characters"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {"results":[{"id":"1","name":"Rick Sanchez","species":"Human"}]}
+                        """));
+    }
+
+}
